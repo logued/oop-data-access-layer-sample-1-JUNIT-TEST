59ff11db3a1029db652015775f7a7366dc208aec
dermo <dermot.logue@dkit.ie>
2024-03-20 16:16:09 +0100

    JUnit Test added  comments.

--- src/test/java/com/dkit/oop/sd2/DAOs/MySqlUserDaoTest.java
+++ src/test/java/com/dkit/oop/sd2/DAOs/MySqlUserDaoTest.java
@@ -1,4 +1,8 @@
 package com.dkit.oop.sd2.DAOs;
+// Note that for this test to work, the database must be created using the original SQL setup file.
+// and the relevant data must not have been changed.
+// Also, MySql server must be running. (XAMPP)      Mar 2024
+//
 
 import com.dkit.oop.sd2.DTOs.User;
 import com.dkit.oop.sd2.Exceptions.DaoException;
